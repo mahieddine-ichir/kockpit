1ea8e0de260da34cf44b2e6d6d5d595213c31bb7
Mahieddine ICHIR <michir@thina.org>
2025-07-02 18:20:23 +0200

    update backend service API

--- kockpit-audit/kockpit-audit-console-ui/src/services/api.js
+++ kockpit-audit/kockpit-audit-console-ui/src/services/api.js
@@ -1,45 +1,44 @@
 import axios from 'axios';
 
-// const API_BASE = import.meta.env.VITE_API_BASE;
-const API_BASE = 'http://localhost:8080/backend/api';
-console.log('API_BASE:', API_BASE);
+//const API_BASE = "https://rcu-apim-dev.azure-api.net/backend/api";
+const API_BASE = "https://kockpit-apim-pro.azure-api.net/backend/api";
+//const API_BASE = 'http://localhost:8080/backend/api';
 
-axios.interceptors.request.use(function (config) {
-  const creds = localStorage.getItem('creds');
-  config.headers.Authorization = `Basic ${creds}`;
-  return config;
-});
-
-export const authenticate = async (username, password) => {
-  console.log(`Authenticating user ${username} on ${API_BASE}/me`);
-  const response = await axios.get(`${API_BASE}/me`, {
-    auth: {
-      username: username,
-      password: password
-    }
-  });
-  return response.data;
-}
+export const fetchAuditReportsForDomainAndEnv = async (domain, env) => {
+  const response = await axios.get(`${API_BASE}/audits/${domain}/${env}`);
+  return response.data.items;
+};
 
 // deprecated use paging one
-export const searchAudits = async (query, domain, env, size, start) => {
-  const response = await axios.get(`${API_BASE}/${domain}/${env}/audits/_search?query=${query}&size=${size}&start=${start}`);
+export const fetchAuditReports = async () => {
+  const response = await axios.get(`${API_BASE}/audit-reports`);
   return response.data;
 };
 
-// fixme remove deprecated
-export const fetchAuditReportsWithPaging = async (domain, env, size, start) => {
-  const response = await axios.get(`${API_BASE}/${domain}/${env}/audits?size=${size}&start=${start}`);
+// deprecated use paging one
+export const searchAudits = async (query) => {
+  const response = await axios.get(`${API_BASE}/audits/_search?query=${query}`);
   return response.data;
 };
 
-export const fetchAuditById = async (id, domain, env) => {
-  const response = await axios.get(`${API_BASE}/${domain}/${env}/audits/${id}`);
+// deprecated
+export const fetchAuditReportsWithPaging = async (size, start) => {
+  const response = await axios.get(`${API_BASE}/audits?size=${size}&start=${start}`);
   return response.data;
 };
 
-export const getConfig = async (domain, appId) => {
-  console.log(`Fetching config from ${API_BASE}/config`);
-  const response = await axios.get(`${API_BASE}/config/${domain}?appId=${appId}`);
+export const fetchAuditReportById = async (id) => {
+  const response = await axios.get(`${API_BASE}/audit-reports/${id}`);
   return response.data;
-}
+};
+
+export const fetchAuditRequests = async (id) => {
+  const response = await axios.get(`${API_BASE}/audit-reports/${id}/requests`);
+  console.log('Audit Requests Response:', JSON.stringify(response.data, null, 2));
+  return response.data;
+};
+
+export const fetchAuditDetails = async (id, traceId) => {
+  const response = await axios.get(`${API_BASE}/audit-reports/${id}/requests/${traceId}`);
+  return response.data;
+};
