f03265e7cb9ae8a14d6971465b3710a667a6a8b0
Mahieddine ICHIR <michir@thina.org>
2025-09-05 09:33:34 +0200

    [audit] trace logs on SimpleLogNotificationService, log warn when eventHub not defined

--- kockpit-audit/kockpit-audit-notification/kockpit-audit-notification-starter-eventhubs/src/main/java/org/kockpit/audit/notification/eventhubs/EventHubAuditServiceAutoConfiguration.java
+++ kockpit-audit/kockpit-audit-notification/kockpit-audit-notification-starter-eventhubs/src/main/java/org/kockpit/audit/notification/eventhubs/EventHubAuditServiceAutoConfiguration.java
@@ -1,6 +1,7 @@
 package org.kockpit.audit.notification.eventhubs;
 
 import com.azure.messaging.eventhubs.EventHubProducerClient;
+import lombok.extern.slf4j.Slf4j;
 import org.kockpit.audit.api.AuditReportNotificationService;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnBean;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
@@ -8,6 +9,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
+@Slf4j
 public class EventHubAuditServiceAutoConfiguration {
 
   @ConditionalOnBean(EventHubProducerClient.class)
@@ -20,6 +22,7 @@ public class EventHubAuditServiceAutoConfiguration {
     @ConditionalOnMissingBean(EventHubProducerClient.class)
     @Bean
     AuditReportNotificationService mockAuditReportNotificationService() {
-        return new MockAuditReportNotificationService();
+      log.warn("Using MockAuditReportNotificationService -> no EventHubProducerClient defined");
+      return new MockAuditReportNotificationService();
     }
 }
