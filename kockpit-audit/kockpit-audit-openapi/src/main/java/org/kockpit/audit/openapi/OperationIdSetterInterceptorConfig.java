a9fb101d7cb5a0bc93b50275cc2730c46812fd32
Cyril Joui <cyril@kisstech.fr>
2025-11-12 11:34:22 +0100

    [NEW] Audit - open api operationId auditor (fix dependency)

--- kockpit-audit/kockpit-audit-openapi/src/main/java/org/kockpit/audit/openapi/OperationIdSetterInterceptorConfig.java
+++ kockpit-audit/kockpit-audit-openapi/src/main/java/org/kockpit/audit/openapi/OperationIdSetterInterceptorConfig.java
@@ -1,7 +1,7 @@
 package org.kockpit.audit.openapi;
 
-import com.accor.wcp.audit.AuditorKeyValueService;
 import lombok.RequiredArgsConstructor;
+import org.kockpit.audit.api.AuditorKeyValueService;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
