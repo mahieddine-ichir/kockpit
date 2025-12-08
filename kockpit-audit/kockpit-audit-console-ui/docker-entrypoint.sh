9b2e00239f976fad8d8f365d3a4d60fb6e97a497
Mahieddine ICHIR <michir@thina.org>
2025-12-08 16:47:25 +0100

    Build & push docker on github

--- kockpit-audit/kockpit-audit-console-ui/docker-entrypoint.sh
+++ kockpit-audit/kockpit-audit-console-ui/docker-entrypoint.sh
@@ -1,7 +1,9 @@
 #!/bin/sh
 # Replace placeholder with actual environment variable
-if [ ! -z "$VITE_API_BASE" ]; then
-  sed -i "s|\${VITE_API_BASE}|$VITE_API_BASE|g" /usr/share/nginx/html/config.js
-fi
+#if [ ! -z "$VITE_API_BASE" ]; then
+#  sed -i "s|\${VITE_API_BASE}|$VITE_API_BASE|g" /usr/share/nginx/html/config.js
+#fi
+
+sed -i "s|\${__KOCKPIT_BACKEND__}|$__KOCKPIT_BACKEND__|g" /etc/nginx/conf.d/default.conf
 
 exec "$@"
