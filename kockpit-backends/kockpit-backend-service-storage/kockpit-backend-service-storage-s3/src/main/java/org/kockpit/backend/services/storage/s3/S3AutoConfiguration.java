f0231cd9431bbd9f4d37156e5723c90925d6c070
Mahieddine ICHIR <michir@thina.org>
2025-12-09 16:02:30 +0100

    Adding s3 implementation

---
+++ kockpit-backends/kockpit-backend-service-storage/kockpit-backend-service-storage-s3/src/main/java/org/kockpit/backend/services/storage/s3/S3AutoConfiguration.java
@@ -0,0 +1,47 @@
+package org.kockpit.backend.services.storage.s3;
+
+import lombok.extern.slf4j.Slf4j;
+import org.kockpit.backend.services.storage.ConfigApiService;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.autoconfigure.AutoConfiguration;
+import org.springframework.context.annotation.Bean;
+import software.amazon.awssdk.auth.credentials.AwsCredentialsProvider;
+import software.amazon.awssdk.auth.credentials.DefaultCredentialsProvider;
+import software.amazon.awssdk.regions.Region;
+import software.amazon.awssdk.services.s3.S3Client;
+
+@AutoConfiguration
+@Slf4j
+public class S3AutoConfiguration {
+
+    @Bean
+    AwsCredentialsProvider awsCredentialsProvider() {
+        return DefaultCredentialsProvider.builder().build();
+    }
+
+    @Bean
+    S3Client s3Client(
+            @Value("${kockpit.sdk.aws.region}") String region,
+            AwsCredentialsProvider credentialsProvider
+    ) {
+        return S3Client.builder()
+                .region(Region.of(region))
+                .credentialsProvider(credentialsProvider)
+                .build();
+    }
+
+    @Bean
+    ConfigApiService s3Repository(
+            S3Client s3Client,
+            @Value("${kockpit.sdk.aws.s3.bucket}") String bucketName,
+            @Value("${kockpit.sdk.aws.region}") String region
+    ) {
+        log.info(
+"""
+    \n
+    - S3 config repository, bucket: {}
+    - Region: {}
+""", bucketName, region);
+        return new S3Repository(s3Client, bucketName);
+    }
+}
