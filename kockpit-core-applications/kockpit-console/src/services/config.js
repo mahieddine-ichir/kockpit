fe44ad314ef687801297895c96852f839cce7bcb
Mahieddine ICHIR <michir@thina.org>
2026-02-18 15:53:52 +0100

    fixing API call from front

--- kockpit-core-applications/kockpit-console/src/services/config.js
+++ kockpit-core-applications/kockpit-console/src/services/config.js
@@ -1,4 +1,5 @@
 export const getApiBase = () => {
     // Try runtime config first, then fallback to build-time env, then default
-    return window.ENV?.VITE_API_BASE || import.meta.env.VITE_API_BASE || '/api';
+    //return window.ENV?.VITE_API_BASE || import.meta.env.VITE_API_BASE || '/api';
+    return '/api'; // fixme for local dev
 }
